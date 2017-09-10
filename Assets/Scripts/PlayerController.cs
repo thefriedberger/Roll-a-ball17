24985fa4e1147e699d9fb594c6adb734cb779862
Tyler Friedberg <thefriedberger@gmail.com>
2017-09-10 17:29:34 -0400

    Player Movement
    Can move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float speed;
+
     private Rigidbody rb;
 
     void Start ()
@@ -18,6 +20,6 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (movement);
+        rb.AddForce (movement * speed);
     }
 }
